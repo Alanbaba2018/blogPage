13beb7daf09cde83d2c45797cebcc3032498d4eb
Alanbaba2018 <1294284048@qq.com>
2019-08-28 00:49:07 +0800

    update

--- src/views/Code/index.vue
+++ src/views/Code/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="code-wrap">
     <el-row type="flex" :gutter="20" style="height: 95%">
-      <el-col :span="10" class="left-panel">
+      <el-col :span="12" class="left-panel">
         <div class="code-editor single-panel" id="monaco" ref="codePanel"></div>
       </el-col>
-      <el-col :span="14" class="right-panel">
+      <el-col :span="12" class="right-panel">
+        <el-button-group class="xCanvas-toolbar" v-show="showToolBar">
+          <el-button size="small" round @click="activeActions('point')">点</el-button>
+          <el-button size="small" round @click="activeActions('polygon')">多边形</el-button>
+          <el-button size="small" round @click="activeActions('line')">线</el-button>
+          <el-button size="small" round @click="activeActions('arrow')">箭头</el-button>
+          <el-button size="small" round @click="activeActions('clear')">清空</el-button>
+        </el-button-group>
         <div class="result-wrap single-panel" id="stage" ref="resultPanel"></div>
       </el-col>
     </el-row>
     <el-row :gutter="20" class="handle-row">
-      <el-col :span="10">
+      <el-col :span="12">
         <el-button type="primary" size="small" class="compile-btn" @click="compile">运行</el-button>
       </el-col>
     </el-row>
@@ -27,13 +34,25 @@ export default {
   name: 'Code',
   data() {
     return {
-      editor: null
+      editor: null,
+      isCompile: false,
+      showToolBar: false,
+      stage: null,
+      crossHLine: '',
+      crossVLine: '',
+      alpha: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'],
+      index: 0,
     };
   },
   mounted() {
+    this.isCompile = this.$route.query.compile === 'false' ? false : true;
+    this.showToolBar = this.$route.query.mode === 'default';
     this.initEditor();
     this.initCode();
     window.addEventListener('resize', this.resize);
+    if (this.showToolBar) {
+      this.initXCanvas();
+    }
   },
   destroyed() {
     window.removeEventListener('resize', this.resize);
@@ -41,18 +60,22 @@ export default {
   methods: {
     resize() {
       this.initEditor();
-      this.compile();
+      if (this.isCompile) {
+        this.compile();
+      }
     },
     initEditor() {
       let codes = '';
       if (this.editor) {
         codes = this.editor.getValue();
       }
-      this.$refs.codePanel.innerHTML = "";
+      const lang = this.$route.query.lang || 'javascript';
+      this.$refs.codePanel.innerHTML = '';
       this.editor = monaco.editor.create(document.getElementById('monaco'), {
         value: null,
-        language: 'javascript'
+        language: lang
       });
+      
       if (codes) {
         this.editor.setValue(codes);
       }
@@ -61,7 +84,9 @@ export default {
       const index = this.$route.params.codeIndex;
       Http.getCodeString(index).then(res => {
         this.editor.setValue(res);
-        this.compile();
+        if (this.isCompile) {
+          this.compile();
+        }
       });
     },
     compile() {
@@ -70,9 +95,119 @@ export default {
       for (let i = childNodes.length - 1; i >= 0; i--) {
         el.removeChild(childNodes[i]);
       }
-      const code = this.editor.getValue()
+      const code = this.editor.getValue();
       const fn = new Function(code);
       fn.call(this);
+    },
+    initXCanvas() {
+      this.stage = new xCanvas.Stage('stage', {zoomChange: 0.5, zoom: 1, maxZoom: 100});
+      this.crossHLine = new xCanvas.Polyline([], {dashArray: [5, 3]}).addTo(this.stage);
+      this.crossVLine = new xCanvas.Polyline([], {dashArray: [5, 3]}).addTo(this.stage);
+    },
+    activeActions(action) {
+      const actions = {
+        point: this.drawPoint,
+        polygon: this.drawPolygon,
+        line: this.drawLine,
+        arrow: this.drawArrow,
+        clear: this.clearAll
+      };
+      if (actions[action]) {
+        actions[action]();
+      }
+    },
+    drawPoint() {
+      const stage = this.stage;
+      stage.once('click', e => {
+        const circle = new xCanvas.Circle(e.pos, 5, { fill: true, color: '#ff0000'});
+        stage.addLayer(circle);
+        this.reset();
+      });
+    },
+    drawPolygon() {
+      const stage = this.stage;
+      const plg = new xCanvas.Polygon([], {fill: false}).addTo(stage);
+      const cache = [];
+      const move = (ev) => {
+        const endPos = ev.pos;
+        plg.setGeometry([...cache, endPos]);
+      };
+      const down = (e) => {
+        if (e.sourceTarget.button === 2) {
+          stage.off('mousedown', down);
+          stage.off('mousemove', move);
+          stage.enableDrag();
+          this.reset();
+          return;
+        }
+        stage.disableDrag();
+        cache.push(e.pos);
+      };
+      stage.on('mousedown', down);
+      stage.on('mousemove', move);
+    },
+    drawLine() {
+      const stage = this.stage;
+      const ply = new xCanvas.Polyline([]).addTo(stage);
+      const cache = [];
+      stage.on('mousedown', e => {
+        cache.push(e.pos);
+        if (e.sourceTarget.button === 2) {
+          stage.off('mousedown');
+          stage.off('mousemove');
+          this.reset();
+          return;
+        }
+        stage.on('mousemove', ev => {
+          ply.setGeometry([...cache, ev.pos]);
+        });
+      })
+    },
+    drawArrow() {
+      const stage = this.stage;
+      stage.on('mousedown', e => {
+        stage.disableDrag();
+        const line = new xCanvas.Polyline([e.pos, e.pos]);
+        const arrow = new xCanvas.Polygon([], { fill: true});
+        const layerGroup = new xCanvas.LayerGroup([line, arrow]).addTo(stage);
+        const text = this.alpha[this.index % this.alpha.length];
+        this.index++;
+        stage.on('mousemove', ev => {
+          line.setLastVertex(ev.pos);
+          arrow.setGeometry(this.getArrowData(...line.getGeometry()));
+        });
+        stage.once('mouseup', () => {
+          stage.off('mousedown');
+          stage.off('mousemove');
+          const bound = layerGroup.getBound();
+          const iText = new xCanvas.IText(bound.getCenter(), text, { color: '#000000', weight: 2}).addTo(stage);
+          stage.enableDrag();
+          this.reset();
+        })
+      })
+    },
+    getArrowData(v1, v2) {
+      const p = new xCanvas.Math.Vector2(v1);
+      const q = new xCanvas.Math.Vector2(v2);
+      const dir = new xCanvas.Math.Vector2(q.x - p.x, q.y - p.y);
+      const oppositDir = dir.clone().rotate(180).normalize();
+      const angle = 60;
+      const len = 10;
+      const p1 = q.clone().add(oppositDir.clone().rotate(angle / 2).scale(len));
+      const p2 = q.clone().add(oppositDir.clone().rotate(-angle / 2).scale(len));
+      return [v2, [p1.x, p1.y], [p2.x, p2.y]];
+    },
+    reset() {
+      this.stage.on('mousemove', e => {
+        const {x, y, width, height } = this.stage.getBound();
+        this.crossHLine.setGeometry([[x, e.pos[1]], [x + width, e.pos[1]]]);
+        this.crossVLine.setGeometry([[e.pos[0], y], [e.pos[0], y + height]]);
+      })
+    },
+    clearAll() {
+      this.stage.clearAllLayers();
+      this.crossHLine = new xCanvas.Polyline([], {dashArray: [5, 3]}).addTo(this.stage);
+      this.crossVLine = new xCanvas.Polyline([], {dashArray: [5, 3]}).addTo(this.stage);
     }
   }
 };
@@ -87,10 +222,20 @@ export default {
     height: 100%;
     border: 1px solid $gray-light-color;
   }
-  .result-wrap {
+  .right-panel {
     position: relative;
-    cursor: pointer;
+    .xCanvas-toolbar {
+      position: absolute;
+      top: 20px;
+      left: 20px;
+      z-index: 100;
+    }
+    .result-wrap {
+      position: relative;
+      cursor: pointer;
+    }
   }
+  
   .handle-row {
     margin-top: 10px;
     text-align: right;
--- src/views/Session/index.vue
+++ src/views/Session/index.vue
@@ -20,144 +20,16 @@
       </section>
       <section data-markdown data-separator="---" data-separator-vertical="--">
         <script type="text/template">
-          ## 基本语法
-          - 数据绑定
-          - 样式绑定
-          - 事件绑定
-          - 修饰符
-          - 过滤器
-          - 指令
-          --
-          ## 数据绑定
-          ```javascript
-          new Vue({
-            el: '#appList',
-            data: {
-              todos: [
-                {text: 'HTML'},
-                {text: 'CSS'},
-                {text: 'javascript'}
-              ]
-            }
-          });
-          ```
-          --
-          ## 样式绑定
-          ```javascript
-          new Vue({
-            el: '#app',
-            data: {
-              classList: ["classA", "classB"]
-            }
-          });
-          ```
-          --
-          ## 事件绑定
-          ```javascript
-          var appEventVue = new Vue({
-            el: '#appEvent',
-            data: {
-              message: 'hello vuejs!'
-            },
-            methods: {
-              reverseMessage: function() {
-                this.message = this.message.split('').reverse().join('');
-              }
-            }
-          });
-          ```
-          --
-          ## 修饰符
-          Note:
-          13是指keyCode为13的时候触发submit。
-          按键名支持，enter, tab, delete, esc, space, down, up ,left, right
-          --
-          ## 过滤器
-          过滤器的作用是在模型的属性指输出或写入时，进行转化。
-          ```javascript
-          Vue.filter('wrap', function (value, begin, end) {
-            return begin + value + end
-          });
-          ```
-          Note:
-          双向指令见demo
-          --
-          ## 指令
-          指令作用是提供一种机制，将数据的变化转化为DOM行为。
-          ```javascript
-          Vue.directive('log', {
-            bind: function () {
-              // 准备工作
-              // 例如，添加事件处理器或只需要运行一次的高耗任务
-            },
-            update: function (newValue, oldValue) {
-              console.log('new value is ', newValue);
-            },
-            unbind: function () {
-              // 清理工作
-              // 例如，删除 bind() 添加的事件监听器
-            }
-          });
-          ```
+          ## 几何基础算法
+          - [点即向量](http://localhost:8080/code/4?lang=typescript&compile=false)
+          - [点在直线判定](http://localhost:8080/code/5?lang=typescript&compile=false&mode=default)
+          - [点在多边形判定](http://localhost:8080/code/6?lang=typescript&compile=false&mode=default)
+          - 两个矩形相交判断 即 判断两个矩形的外包矩形的宽高是否小于两个矩形宽高分别相加
+          - .....
         </script>
       </section>
       <section data-markdown data-separator="---" data-separator-vertical="--">
-        <script type="text/template">
-          ## 组件化
-          - 注册和使用
-          - props
-          - 父子组件通信
-          --
-          ## 注册和使用
-          ```javascript
-          // 创建
-          var MyComponent = Vue.extend({
-            // 选项...
-            template: '<div>A custom component!</div>'
-          });
-          // 注册
-          Vue.component('demo', MyComponent)
-          ```
-          --
-          ## 父子组件通信
-          子组件通过事件冒泡通知父组件状态变化。
-          ```javascript
-          Vue.component('child', {
-            template: '#child',
-            data: function () {return { msg: 'hello' }},
-            methods: { notify: function () {
-                this.$dispatch('child-msg', this.msg)
-            } }
-          });
-          new Vue({
-            el: '#parent',
-            data: { messages: [] },
-            events: {'child-msg': function (msg) {
-                this.messages.push(msg)
-            } }
-          });
-          ```
-        </script>
-      </section>
-      <section data-markdown data-separator="---" data-separator-vertical="--">
-        <script type="text/template">
-          ## 大型应用构建
-          - 单文件组件
-          - 插件
-          --
-          ## 单文件组件
-          使用vue文件去包含一个组件的全部内容。
-          ![vue文件](http://cn.vuejs.org/images/vue-component.png)
-          --
-          ## 插件
-          - vue-resource
-          - vue-devtools
-          - vue-router
-          - vue-validator
-          Note:
-          vue-resource: ajax加载；vue-devtools: chrome开发者工具；vue-router 单页面路由;vue-validator 表单验证。
-
-        </script>
+        ## Canvas交互
       </section>
       <section>
         <h2>Q & A </h2>
