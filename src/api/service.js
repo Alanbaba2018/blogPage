4c9b0439c18daed72fcde63c26f331bd1ee34f1b
Alanbaba2018 <1294284048@qq.com>
2019-08-28 00:52:12 +0800

    sad

--- src/api/service.js
+++ src/api/service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Message } from 'element-ui';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: 'http://localhost:8080/',
   timeout: 150000
 });
 
